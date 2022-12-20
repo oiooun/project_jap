911a96004324fcd64cb0c4720d3c95835c58dd26
SongyeonK <117712815+SongyeonK@users.noreply.github.com>
2022-12-21 04:46:32 +0900

    Update viz.py

--- function/viz.py
+++ function/viz.py
@@ -1,87 +1,52 @@
 import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+## 1. 대시보드에 각 데이터 항목별로 시각화
+
+# 캡스톤 프로젝트 설문 자료 불러오기
+job = pd.read_excel('캡스톤_설문_final.xlsx')
+
+
+# 전공만족도에 따른 전공학점 scatter 그래프로 시각화
+plt.figure(figsize=(13, 11))
+x = job['major_satisfaction']  # x축: 전공만족도
+y = job['major_grade']         # y축: 전공학점
+plt.scatter(x, y)              # scatter 그래프
+plt.show()                     # 그래프 출력
+
+# # 두 등급으로 구분된 데이터 프레임 만들기
+job['employment'] = [1 if i >= 2 else 0 for i in job['employment_status']]   # 취업 상태를 기준으로 한 class column 생성
+print(job)
+
+# 두 등급으로 구분된 설문 데이터 항목 간의 pair plot
+job_data = job.drop(['타임스탬프', 'identity', 'age', 'major', 'employment_status'],axis=1)   # 가설검증에 필요하지 않은 데이터 열 삭제
+sns.pairplot(job_data, hue='employment',markers=['o', 's'])  # sns tool 이용
+plt.show()  # 그래프 출력
+
+
+## 2. 데이터 연관도를 기반으로 한 시각화
+
 
 # 데이터셋 불러오기
-survey_result = pd.read_excel("C:/Users/syk19/PycharmProjects/pythonProject21/캡스톤_설문_final.xlsx", sheet_name="설문응답")
-
+survey_result = pd.read_excel("캡스톤_설문_final.xlsx", sheet_name="설문응답")
+# survey_result_df1 = survey_result[['major_satisfaction', 'major_grade']]
 
 # 한글 글씨 깨짐 해결
 matplotlib.rcParams['font.family'] ='Malgun Gothic'
 matplotlib.rcParams['axes.unicode_minus'] =False
 
+# 각 데이터컬럼간 연관도 계산(corr)
+corr_df = survey_result.corr(method='pearson')
+print(corr_df)
 
+# 그래프 크기 설정
+plt.figure(figsize=(15, 8))
+plt.subplot(1, 1, 1)
 
-# 1. 전공만족도와 전공학점 간의 상관관계를 그래프로 나타냄 (종속변수(y): 전공학점, 고정변인(x): 전공만족도)
-#survey_result_df1 = survey_result[(survey_result['major_satisfaction']) | (survey_result['major_grade'])]
-survey_result_df1 = survey_result[['major_satisfaction', 'major_grade']]
-# survey_result_df1 = survey_result_df1.set_index('major_satisfaction')
-print(survey_result_df1)
-# survey_result_df1.plot.scatter(x='major_satisfaction', y='major_grade')
-
-plt.figure(figsize=(200, 100))
-plt.subplot(2, 2, 1)
-# plt.plot(survey_result_df1)
-plt.scatter(x=survey_result_df1['major_satisfaction'], y=survey_result_df1['major_grade'], color='blue', alpha=0.4, s=150)
-plt.title('전공만족도와 전공학점 간의 상관관계')  # 제목
-plt.xlim([0,6])
-plt.ylim([0,7])
-plt.xlabel('전공만족도')  # x축레이블
-plt.ylabel('전공학점')  # y축레이블
-
-
-
-# 2. 전공학점과 취업자신감 간의 상관관계를 그래프로 나타냄 (종속변수(y): 취업자신감, 고정변인(x): 전공학점)
-
-survey_result_df2 = survey_result[['major_grade', 'job_confidence']]
-# survey_result_df1 = survey_result_df1.set_index('major_satisfaction')
-print(survey_result_df2)
-# survey_result_df1.plot.scatter(x='major_satisfaction', y='major_grade')
-
-plt.figure(figsize=(200, 100))
-plt.subplot(2, 2, 2)
-# plt.plot(survey_result_df1)
-plt.scatter(x=survey_result_df2['major_grade'], y=survey_result_df2['job_confidence'], color='blue', alpha=0.4, s=150)
-plt.title('전공학점과 취업자신감 간의 상관관계')  # 제목
-plt.xlim([0,7])
-plt.ylim([0,7])
-plt.xlabel('전공학점')  # x축레이블
-plt.ylabel('취업자신감')  # y축레이블
-
-
-# 3. 전공만족도와 취업상태 간의 상관관계를 그래프로 나타냄 (종속변수(y): 취업상태, 고정변인(x): 전공만족도)
-
-survey_result_df3 = survey_result[['major_satisfaction', 'employment_status']]
-# survey_result_df1 = survey_result_df1.set_index('major_satisfaction')
-print(survey_result_df3)
-# survey_result_df1.plot.scatter(x='major_satisfaction', y='major_grade')
-
-plt.figure(figsize=(200, 100))
-plt.subplot(2, 2, 3)
-# plt.plot(survey_result_df1)
-plt.scatter(x=survey_result_df3['major_satisfaction'], y=survey_result_df3['employment_status'], color='blue', alpha=0.4, s=150)
-plt.title('전공만족도와 취업 간의 상관관계')  # 제목
-plt.xlim([0,6])
-plt.ylim([0,4])
-plt.xlabel('전공만족도')  # x축레이블
-plt.ylabel('취업')  # y축레이블
-
-# 4. 대학생활만족도와 취업상태 간의 상관관계를 그래프로 나타냄 (종속변수(y): 취업상태, 고정변인(x): 대학생활만족도)
-
-survey_result_df4 = survey_result[['univ_life_satisfaction', 'employment_status']]
-# survey_result_df1 = survey_result_df1.set_index('major_satisfaction')
-print(survey_result_df4)
-# survey_result_df1.plot.scatter(x='major_satisfaction', y='major_grade')
-
-plt.figure(figsize=(200, 100))
-plt.subplot(2, 2, 4)
-# plt.plot(survey_result_df1)
-plt.scatter(x=survey_result_df4['univ_life_satisfaction'], y=survey_result_df4['employment_status'], color='blue', alpha=0.4, s=150)
-plt.title('전공만족도와 전공학점 간의 상관관계')  # 제목
-plt.xlim([0,6])
-plt.ylim([0,4])
-plt.xlabel('전공만족도')  # x축레이블
-plt.ylabel('전공학점')  # y축레이블
-
+# seaborn을 이용하여 히트맵 생성
+sns.heatmap(corr_df, annot=True, cmap='Blues')
 
 plt.show()
