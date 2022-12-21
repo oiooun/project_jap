73f11b4c5751da047807f46ecbe4775137ed72b5
SongyeonK <117712815+SongyeonK@users.noreply.github.com>
2022-12-21 17:29:55 +0900

    Add files via upload

--- function/viz.py
+++ function/viz.py
@@ -1,51 +1,67 @@
-import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
+# package import
+import requests                         
+from pandas import DataFrame
+from bs4 import BeautifulSoup
+import re
+from datetime import datetime
+import os
+
+# 현재 시간 저장
+time = str(datetime.now())        # 엑셀 저장 시 크롤링한 날짜, 시간을 파일명에 넣기 위해 저장
+time = time[:time.rfind(':')].replace(' ', '_')
+time = time.replace(':', '시') + '분'
+
+# input 생성
+query = input('검색 키워드를 입력하세요 : ')  # 검색할 키워드, 추출할 뉴스 기사 수 저장
+news_num = int(input('총 필요한 뉴스기사 수를 입력해주세요(숫자만 입력) : '))
+query = query.replace(' ', '+')  # query에서 ' ' 를 '+'로 바꾸어주는 이유는 띄어쓰기 시 URL 조건 절에 '+'로 적용되어 요청 인자가 들어가기 때문.
 
 
-## 1. 대시보드에 각 데이터 항목별로 시각화
+# 요청할 URL 생성 및 요청
+news_url = 'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={}' # 키워드(query)를 URL의 조건절 중 키워드에 해당하는 변수에 대응시켜 요청 URL 생성.
 
-# 캡스톤 프로젝트 설문 자료 불러오기
-job = pd.read_excel('캡스톤_data_final.xlsx')
-
-# 전공만족도에 따른 전공학점 scatter 그래프로 시각화
-plt.figure(figsize=(13, 11))
-x = job['major_satisfaction']  # x축: 전공만족도
-y = job['major_grade']         # y축: 전공학점
-plt.scatter(x, y)              # scatter 그래프
-plt.show()                     # 그래프 출력
-
-# # 두 등급으로 구분된 데이터 프레임 만들기
-job['employment'] = [1 if i >= 2 else 0 for i in job['employment_status']]   # 취업 상태를 기준으로 한 class column 생성 (employment_status가 1일때 취업한 상태, 2일때 취업 안한 상태)
-print(job)
-
-# 두 등급으로 구분된 설문 데이터 항목 간의 pair plot
-job_data = job.drop(['타임스탬프', 'identity', 'age', 'major', 'employment_status'],axis=1)   # 가설검증에 필요하지 않은 데이터 열 삭제
-sns.pairplot(job_data, hue='employment',markers=['o', 's'])  # sns tool 이용
-plt.show()  # 그래프 출력
+req = requests.get(news_url.format(query))      # request package의 get 함수를 이용하여 HTML 코드 받기
+soup = BeautifulSoup(req.text, 'html.parser')   # 받은 코드를 bs4의 BeautifulSoup 함수를 이용하여 파싱
 
 
-## 2. 데이터 연관도를 기반으로 한 시각화
+# 원하는 정보를 담을 변수 생성 (key : 번호, value : 뉴스 기사 정보)
+news_dict = {}  # 딕셔너리 형태
+idx = 0  # idx : 현재 뉴스의 번호
+cur_page = 1  # cur_page : 네이버 뉴스의 웹 페이지
 
+print()
+print('크롤링 중...')
 
-# 데이터셋 불러오기
-survey_result = pd.read_excel("캡스톤_data_final.xlsx", sheet_name="설문응답")
-# survey_result_df1 = survey_result[['major_satisfaction', 'major_grade']]
+# HTML 코드에서 태그를 이용하여 원하는 정보 탐색
+while idx < news_num:
+    table = soup.find('ul', {'class': 'list_news'})
+    li_list = table.find_all('li', {'id': re.compile('sp_nws.*')})
+    area_list = [li.find('div', {'class': 'news_area'}) for li in li_list]
+    a_list = [area.find('a', {'class': 'news_tit'}) for area in area_list]
 
-# 한글 글씨 깨짐 해결
-matplotlib.rcParams['font.family'] ='Malgun Gothic'
-matplotlib.rcParams['axes.unicode_minus'] =False
+    for n in a_list[:min(len(a_list), news_num - idx)]:
+        news_dict[idx] = {'title': n.get('title'),  # news_dict : 뉴스 기사를 담는 딕셔너리
+                          'url': n.get('href')}
+        idx += 1
 
-# 각 데이터컬럼간 연관도 계산(corr)
-corr_df = survey_result.corr(method='pearson')
-print(corr_df)
+    cur_page += 1
 
-# 그래프 크기 설정
-plt.figure(figsize=(15, 8))
-plt.subplot(1, 1, 1)
+    pages = soup.find('div', {'class': 'sc_page_inner'})
+    next_page_url = [p for p in pages.find_all('a') if p.text == str(cur_page)][0].get('href') # 현재 수집한 뉴스 기사 수가 부족한 경우 다음 페이지로 넘어가야 하므로 다음 페이지에 해당하는 URL을 추출
 
-# seaborn을 이용하여 히트맵 생성
-sns.heatmap(corr_df, annot=True, cmap='Blues')
+    req = requests.get('https://search.naver.com/search.naver' + next_page_url)
+    soup = BeautifulSoup(req.text, 'html.parser')
 
-plt.show()
+# 최종 데이터 생성 => 데이터 프레임 변환 및 저장
+print('크롤링 완료')
+
+print('데이터프레임 변환')
+news_df = DataFrame(news_dict).T  # 딕셔너리(news_dict)를 데이터 프레임(news_df)으로 변환
+
+folder_path = os.getcwd()
+xlsx_file_name = '네이버뉴스_{}_{}.xlsx'.format(query, time)  # 크롤링한 키워드(query)와 크롤링 날짜(date)를 엑셀 파일 명으로 하여 저장
+
+news_df.to_excel(xlsx_file_name)
+
+print('엑셀 저장 완료 | 경로 : {}\\{}'.format(folder_path, xlsx_file_name))
+os.startfile(folder_path)
